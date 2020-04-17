c9b0ce0f117584435a8a0feefc1fb7b219e1ccd3
annmwas <annmichael.w@gmail.com>
2020-04-17 19:07:19 +0300

    Add files via upload

---
+++ html/search.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Search Results</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/bootstrap-responsive.css" type="text/css" media="screen">    
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+
+<script type="text/javascript" src="js/jquery.js"></script>  
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/superfish.js"></script>
+
+<script type="text/javascript" src="js/jquery.ui.totop.js"></script>
+
+<script type="text/javascript" src="search/search.js"></script>
+
+<script type="text/javascript" src="js/script.js"></script>
+<script>
+$(document).ready(function() {
+	//
+	
+
+}); //
+$(window).load(function() {
+	//
+
+}); //
+</script>		
+<!--[if lt IE 8]>
+		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+	<![endif]-->    
+
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>      
+  <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+<![endif]-->
+</head>
+
+<body class="subpage">
+<div id="main">
+<div class="menu_bg_wrapper">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="menu_bg_inner"><div class="menu_bg"></div></div>	
+</div>	
+</div>	
+</div>	
+</div>
+<header>
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="header_inner clearfix">
+	<div class="logo_wrapper"><a href="index.html" class="logo"><img src="images/logo.png" alt=""></a></div>	
+	<div class="menu_wrapper">
+		<div class="navbar navbar_">
+			<div class="navbar-inner navbar-inner_">
+				<a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">main menu</a>
+				<div class="nav-collapse nav-collapse_ collapse">
+					<ul class="nav sf-menu clearfix">
+						<li><a href="index.html">home</a></li>				
+						<li><a href="index-1.html">news</a></li>	
+						<li><a href="index-2.html">hot tours</a></li>							
+						<li class="sub-menu sub-menu-1"><a href="index-3.html">destinations<em></em></a>
+							<ul>						
+								<li class="sub-menu sub-menu-2"><a href="index-3.html">Asia</a>
+									<ul>
+										<li><a href="index-3.html">lorem ipsum dolor</a></li>
+										<li><a href="index-3.html">sit amet</a></li>
+										<li><a href="index-3.html">adipiscing elit</a></li>
+										<li><a href="index-3.html">nunc suscipit</a></li>
+										<li><a href="404.html">404 page not found</a></li>
+									</ul>
+								</li>
+								<li><a href="index-3.html">Europe</a></li>
+								<li><a href="index-3.html">America</a></li>																	
+								<li><a href="index-3.html">Africa</a></li>
+								<li><a href="index-3.html">Australia</a></li>
+								<li><a href="index-3.html">Islands</a></li>
+							</ul>						
+						</li>
+						<li><a href="index-4.html">services</a></li>	
+						<li><a href="index-5.html">gallery</a></li>																	
+						<li><a href="index-6.html">contacts</a></li>											
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>	
+</div>	
+</div>	
+</div>	
+</header>	
+
+<div id="slider">
+<div class="slider_bot">
+	<div class="slider_bot1">
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<div class="slider_bot1_inner">
+						<div class="search-form-wrapper clearfix">
+							<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="navbar-form" >
+								<input type="text" name="s" value='Search tours right now:' onBlur="if(this.value=='') this.value='Search tours right now:'" onFocus="if(this.value =='Search tours right now:' ) this.value=''">
+								<a href="#" onClick="document.getElementById('search-form').submit()">find!</a>
+							</form>	
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="slider_bot2">
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+					<div class="slider_bot2_inner clearfix">
+						<div class="banner0">
+							<a href="index-2.html"><img src="images/banner0.png" alt="" class="img"></a>
+						</div>
+						<ul class="banners clearfix">
+							<li>
+								<a href="index-2.html">
+									<div class="txt1">Luxury Hotels</div>
+									<div class="txt2">Lorem ipsum dolor sit amet conse ctetur.</div>
+								</a>
+							</li>
+							<li>
+								<a href="index-3.html">
+									<div class="txt1">Top Destinations</div>
+									<div class="txt2">Lorem ipsum dolor sit amet conse ctetur.</div>
+								</a>
+							</li>
+							<li>
+								<a href="index-5.html">
+									<div class="txt1">Photo Gallery</div>
+									<div class="txt2">Lorem ipsum dolor sit amet conse ctetur.</div>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>	
+<div class="img"><img src="images/slide_page3.jpg" alt=""></div>
+</div>	
+
+
+
+<div id="content">
+<div class="container">
+<div class="row">
+	<div class="span12">
+		
+		<h1>Search result:</h1>
+
+		<div id="search-results"></div>
+
+
+
+
+	</div>	
+</div>	
+</div>	
+</div>
+
+<footer>
+<div class="container">
+<div class="row">
+<div class="span12">
+	<div class="bot1 clearfix">
+		<img src="images/plane1.png" alt="" class="plane1">		
+		<img src="images/boat1.png" alt="" class="boat1">		
+		<div class="social_txt1">Follow us</div>
+		<div class="social_wrapper">
+			<ul class="social clearfix">
+			    <li><a href="#"><img src="images/social_ic1.png"></a></li>
+			    <li><a href="#"><img src="images/social_ic2.png"></a></li>
+			    <li><a href="#"><img src="images/social_ic3.png"></a></li>
+			    <li><a href="#"><img src="images/social_ic4.png"></a></li>
+			    <li><a href="#"><img src="images/social_ic5.png"></a></li>
+			    <li><a href="#"><img src="images/social_ic6.png"></a></li>
+			    
+			</ul>
+		</div>		
+	</div>
+	<div class="bot2 clearfix">
+		<div class="menu_bot">
+	        <ul id="menu_bot" class="clearfix">
+	          <li><a href="index.html">home</a></li>
+	          <li><a href="index-1.html">news</a></li>
+	          <li><a href="index-2.html">hot tours</a></li>
+	          <li><a href="index-3.html">destinations</a></li>
+	          <li><a href="index-4.html">services</a></li>
+	          <li><a href="index-5.html">gallery</a></li>          
+	          <li><a href="index-6.html">contacts</a></li>          
+	        </ul>
+		</div>
+		<div class="copyright">Copyright © 2013. All rights reserved. &nbsp; <a href="#">Privacy policy</a></div>
+	</div>
+</div>	
+</div>	
+</div>	
+</footer>
+
+
+
+
+
+
+
+
+	
+</div>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
